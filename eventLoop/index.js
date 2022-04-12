03f022b67f8584a7217ae71235a7947cd113d380
gbh10c <grandcasa10@gmail.com>
2022-04-12 19:49:28 -0400

    Event loop exercise

---
+++ eventLoop/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+process.on('beforeExit', () => {
+  console.log("Print Fifth");
+});
+
+fs.readFile(__filename, () => {
+  setImmediate(() => {
+    console.log("Print Third");
+  });
+});
+
+setImmediate(() => {
+  console.log('Print Second');
+});
+
+console.log('Print First');
+
+setTimeout(() => {
+console.log("Print Fourth");
+}, 2000);
