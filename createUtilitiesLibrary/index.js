7321ab85ede7b3903f13c3bcbaef21195b837bcc
gbh10c <grandcasa10@gmail.com>
2022-04-12 20:57:57 -0400

    Updated import statement to use file.function syntax for one utilities folder

--- createUtilitiesLibrary/index.js
+++ createUtilitiesLibrary/index.js
@@ -1,11 +1,11 @@
-const { sum, concat } = require('./utilities/combine.js');
+const combine = require('./utilities/combine.js');
 const { lgNum, cut3 } = require('./utilities/identify.js');
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [5, 6, 7, 8];
 
-console.log(`Sum of numbers in an arry: ${sum(arr1)}`);
-console.log(`Concatenate two arrays: ${concat(arr1, arr2)}`);
+console.log(`Sum of numbers in an arry: ${combine.sum(arr1)}`);
+console.log(`Concatenate two arrays: ${combine.concat(arr1, arr2)}`);
 console.log(`Largest number in an array: ${lgNum(arr2)}`);
 console.log(`Remove third number from an array: ${cut3(arr1)}`);
 
