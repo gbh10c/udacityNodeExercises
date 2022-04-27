95cabe7adc0f21b72b2b3709f0e4212f53066262
gbh10c <grandcasa10@gmail.com>
2022-04-27 15:29:06 -0400

    Updated with mapped object and JSON file output

--- expressRouterApi/src/index.ts
+++ expressRouterApi/src/index.ts
@@ -1,10 +1,31 @@
 import express from 'express';
-import routes from './routes/index';
+import { promises as fsPromises } from 'fs';
+import csv from 'csvtojson';
 
 const app = express();
 const port = 3000;
 
-app.use('/', routes);
+const inputFile = './users.csv';
+const outputFile = 'users.json';
+
+app.get('/convert', (req, res) => {
+  res.send('Converting in progress!');
+  csv()
+    .fromFile(inputFile)
+    .then((data)=>{
+      let newData = data.map( (item: {
+        first_name: string; last_name: string; phone: string; }) => {
+          let first = item.first_name;
+          let last = item.last_name;
+          let phone = item.phone;
+          if (item.phone === "") {
+            phone = "missing data";
+          }
+          return {first, last, phone};
+        });
+      fsPromises.writeFile(outputFile, JSON.stringify(newData));
+    });
+});
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
